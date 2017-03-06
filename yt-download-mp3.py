d2ba93762e5b18f18e1f5901bd11f2250fd74913
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-03-06 01:39:18 +0100

    YouTube download scripts

---
+++ yt-download-mp3.py
@@ -0,0 +1,29 @@
+#
+# Downloads YouTube video/playlist in
+# highest audio quality and converts it to mp3
+# Requires youtube-dl, ffprobe and ffmpeg
+#
+
+import os
+
+print("YouTube mp3 download")
+print("Provide url (single video or playlist):")
+url = input()
+
+print("Select output format:")
+print("1. Standard")
+print("2. Album")
+print("3. ID")
+style = input()
+
+formats = {
+	'1': '%(title)s.%(ext)s',
+	'2': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+	'3': '%(id)s.%(ext)s'
+}
+
+if formats[style] is None:
+	output_format = formats[1]
+else:
+	output_format = formats[style]
+os.system("youtube-dl -x -i -o \"{}\" --audio-format mp3 --audio-quality 0 {}".format(output_format, url))
---
+++ yt-download.py
@@ -0,0 +1,30 @@
+#
+# Downloads YouTube video/playlist in highest quality as mp4
+# Requires youtube-dl
+#
+
+import os
+
+print("YouTube download")
+print("Provide url (single video or playlist):")
+url = input()
+
+print("Select output format:")
+print("1. Standard")
+print("2. Album")
+print("3. ID")
+print("4. Playlist")
+style = input()
+
+formats = {
+	'1': '%(title)s.%(ext)s',
+	'2': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+	'3': '%(id)s.%(ext)s',
+	'4': '%(playlist)s/%(title)s.%(ext)s'
+}
+
+if formats[style] is None:
+	output_format = formats[1]
+else:
+	output_format = formats[style]
+os.system("youtube-dl -f mp4 -i -o \"{}\" {}".format(output_format, url))
