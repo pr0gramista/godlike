3cd21d6bd8dc4ee297a99cb9edced4c2cd7c8e76
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-12-06 00:04:32 +0100

    Update YouTube scripts to work without interaction

--- yt-download-mp3.py
+++ yt-download-mp3.py
@@ -6,27 +6,61 @@ Requires youtube-dl, ffprobe and ffmpeg.
 """
 
 import os
-
-print("YouTube mp3 download")
-print("Provide url (single video or playlist):")
-url = input()
-
-print("Select output format:")
-print("1. Standard")
-print("2. Album")
-print("3. ID")
-style = input()
+import sys
 
 formats = {
     '1': '%(title)s.%(ext)s',
     '2': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
     '3': '%(id)s.%(ext)s'
 }
+formats_long = {
+    'standard': '%(title)s.%(ext)s',
+    'album': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+    'id': '%(id)s.%(ext)s'
+}
 
-if style in formats:
-    output_format = formats[style]
+# Interactive mode
+if '-i' in sys.argv or '-interactive' in sys.argv:
+    print("YouTube mp3 download")
+    print("Provide url (single video or playlist):")
+    url = input()
+
+    print("Select output format:")
+    print("1. Standard")
+    print("2. Album")
+    print("3. ID")
+    style = input()
+
+    formats = {
+        '1': '%(title)s.%(ext)s',
+        '2': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+        '3': '%(id)s.%(ext)s'
+    }
+
+    if style in formats:
+        output_format = formats[style]
+    else:
+        output_format = formats['1']
+
+    os.system("youtube-dl --download-archive downloads-mp3.txt -x -i -o \"{}\" --audio-format "
+              "mp3 --audio-quality 0 {}"
+              .format(output_format, url))
+elif '-h' in sys.argv or '-help' in sys.argv or len(sys.argv) == 0:
+    print(
+        "{}\n"
+        "-i -interactive\n"
+        "-f -format [standard, album, id]\n"
+        "[url]".format(sys.argv[0]))
 else:
-    output_format = formats['1']
+    format = 'standard'
+    if '-f' in sys.argv:
+        format_index = sys.argv.index('-f') + 1
+        if len(sys.argv) > format_index:
+            format = sys.argv[format_index]
 
-os.system("youtube-dl --download-archive downloads-mp3.txt -x -i -o \"{}\" --audio-format mp3 --audio-quality 0 {}"
-          .format(output_format, url))
+    url = sys.argv[-1]
+    output_format = formats_long[format]
+
+    os.system("youtube-dl --download-archive downloads-mp3.txt -x -i -o \"{}\" "
+              "--audio-format mp3 --audio-quality 0 {}"
+              .format(output_format, url))
--- yt-download.py
+++ yt-download.py
@@ -4,17 +4,7 @@ Requires youtube-dl
 """
 
 import os
-
-print("YouTube download")
-print("Provide url (single video or playlist):")
-url = input()
-
-print("Select output format:")
-print("1. Standard")
-print("2. Album")
-print("3. ID")
-print("4. Playlist")
-style = input()
+import sys
 
 formats = {
     '1': '%(title)s.%(ext)s',
@@ -22,10 +12,46 @@ formats = {
     '3': '%(id)s.%(ext)s',
     '4': '%(playlist)s/%(title)s.%(ext)s'
 }
+formats_long = {
+    'standard': '%(title)s.%(ext)s',
+    'album': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+    'id': '%(id)s.%(ext)s',
+    'playlist': '%(playlist)s/%(title)s.%(ext)s'
+}
 
-if style in formats:
-    output_format = formats[style]
+# Interactive mode
+if '-i' in sys.argv or '-interactive' in sys.argv:
+    print("YouTube download")
+    print("Provide url (single video or playlist):")
+    url = input()
+
+    print("Select output format:")
+    print("1. Standard")
+    print("2. Album")
+    print("3. ID")
+    print("4. Playlist")
+    style = input()
+
+    if style in formats:
+        output_format = formats[style]
+    else:
+        output_format = formats['1']
+
+    os.system("youtube-dl -f mp4 -i -o \"{}\" {}".format(output_format, url))
+elif '-h' in sys.argv or '-help' in sys.argv or len(sys.argv) == 0:
+    print(
+        "{}\n"
+        "-i -interactive\n"
+        "-f -format [standard, album, id, playlist]\n"
+        "[url]".format(sys.argv[0]))
 else:
-    output_format = formats['1']
+    format = 'standard'
+    if '-f' in sys.argv:
+        format_index = sys.argv.index('-f') + 1
+        if len(sys.argv) > format_index:
+            format = sys.argv[format_index]
 
-os.system("youtube-dl -f mp4 -i -o \"{}\" {}".format(output_format, url))
+    url = sys.argv[-1]
+    output_format = formats_long[format]
+
+    os.system("youtube-dl -f mp4 -i -o \"{}\" {}".format(output_format, url))
