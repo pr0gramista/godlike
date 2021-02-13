6a34a8d38e9bdb846681f44f03c49ce875847df0
Bartosz Wiśniewski <kontakt@pr0gramista.pl>
2021-02-13 02:20:28 +0100

    Delete hugo-mk-post.py

--- hugo-mk-post.py
+++
@@ -1,9 +0,0 @@
-"""Shortcut for making post."""
-import os
-
-print("Hugo make post")
-print("Provide slug:")
-slug = input()
-
-os.system("hugo new post/{}.md".format(slug))
-os.system("atom content/post/{}.md".format(slug))
